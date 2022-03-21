070891654597266c56b5f5a3ac75ef2475a2af77
saribalazs <71698313+saribalazs@users.noreply.github.com>
2022-03-21 16:31:53 +0100

    Update paddle.py

--- paddle.py
+++ paddle.py
@@ -25,21 +25,3 @@ class Paddle(pygame.sprite.Sprite):
         if self.rect.y > 400:
           self.rect.y = 400
             
-class Labda(pygame.sprite.Sprite):
-    def __init__(self, color, width, height):
-        super().__init__()
-        
-        self.image = pygame.Surface([width, height])
-        self.image.fill(fekete) 
-        self.image.set_colorkey(fekete)
-        
-        pygame.draw.rect(self.image, color, [0, 0, width, height])
-        
-        self.velocity = [randint(4,8),randint(-8,8)]
-        
-        self.rect = self.image.get_rect()
-        
-    def update(self):
-        self.rect.x += self.velocity[0]
-        self.rect.y += self.velocity[1]
-        
